aa44cb7ee117bdbe3164e2a1df0d5b8853fca364
GABRIEL ALVES BESERRA DO VALE <395784554@alunos.itaqua.local>
2020-02-21 21:56:45 -0300

    Criação de arquivo de login e cadastro para demonstração de metodos HTTP GET E POST

---
+++ logar.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8"/>
+
+<?php
+    $nomeDoUsuario = $_POST["nome"];
+    $cpfDoUsuario = $_POST["cpf"];
+    $idadeDoUsuario = $_POST["idade"];
+
+    echo "$nomeDoUsuario, Você foi Registrado com sucesso!!!!"
+?>
